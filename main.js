6fd6c16f5990a96ff5758fe08e105a04c27f72a0
Terry Herndon <terryherndon@Terrys-MacBook-Pro-2.local>
2018-12-29 22:44:35 -0500

    Finished first mockup (at my grandparent's house)

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log(1492)
+window.onload = function() {
+  document.getElementById('img1').onclick=function() {
+    document.getElementById('img1').src = 'pics/pic2.png'
+  }
+
+  var codes = []
+
+  document.getElementById('submitBtn').onclick = function() {
+    code = document.getElementById('textfield').value
+    if (!codes.includes(code)) {
+      var image = document.createElement('img')
+      image.src = 'pics/'+ code +'.png'
+      if (image.height) {
+        codes[codes.length] = code
+        document.body.appendChild(document.createElement('br'))
+        document.body.appendChild(image)
+      }
+    }
+  }
+}
